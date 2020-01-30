bb26129b61ce3aa328f24f7587df85d152ce5d32
alexahs <alexander.harold.sexton@gmail.com>
2020-01-30 16:48:13 +0100

    test

--- project1/main.cpp
+++ project1/main.cpp
@@ -2,9 +2,8 @@
 #include "Hamiltonians/hamiltonian.h"
 
 
-using namespace std;
 
 int main() {
-    cout << "hello\n4*2 = " << hey(4) << endl;
+    std::cout << "hello\n4*2 = " << hey(4) << endl;
     return 0;
 }
