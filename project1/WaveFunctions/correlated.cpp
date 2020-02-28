343bd3b55a6daf93d20de44a84237cd8808d74fc
alexahs <alexander.harold.sexton@gmail.com>
2020-02-28 13:50:09 +0100

    start skeleton code of correlated wavefunction

---
+++ project1/WaveFunctions/correlated.cpp
@@ -0,0 +1,14 @@
+#include "wavefunction.h"
+#include <cmath>
+#include <cassert>
+
+
+Correlated::Correlated(System* system, double alpha, double beta) :
+    WaveFunction(system) {
+    assert(alpha >= 0);
+    assert(beta >= 0);
+    m_numberOfParameters = 2;
+    m_parameters.reserve(2);
+    m_parameters.push_back(alpha);
+    m_parameters.push_back(beta);
+}
