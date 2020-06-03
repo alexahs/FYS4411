7f6103d16bbdfd23bca4f2706af36ea4e7428057
gabrielscabrera <gabriel.sigurd.cabrera@gmail.com>
2020-06-03 12:15:49 +0200

    Ayy

--- project2/PostProcessing/processing.py
+++ project2/PostProcessing/processing.py
@@ -172,8 +172,8 @@ def load_energy_grids(savename):
 if __name__ == '__main__':
     energies = read_outputs.read_energy_samples()
     sorted_results = get_energy_grids(energies, test = True)
-    save_energy_grids(sorted_results, 'test')
-    loaded_results = load_energy_grids('test')
+    save_energy_grids(sorted_results, 'run_2')
+    loaded_results = load_energy_grids('run_2')
     for i,j in zip(sorted_results.items(), loaded_results.items()):
         assert np.array_equal(i[1]['sample'], j[1]['sample'])
         assert np.array_equal(i[1]['blocking'], j[1]['blocking'])
